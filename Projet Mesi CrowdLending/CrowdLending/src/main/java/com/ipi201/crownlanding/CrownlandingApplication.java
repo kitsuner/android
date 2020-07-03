54115a7a48d30972ce5bf9c7bb1e0eb32eba62de
Fabrice René-Corail <45557264+FabriceReneCorail@users.noreply.github.com>
2020-07-03 11:49:49 +0200

    Add files via upload
    Socle Back du projet CrowdLending

---
+++ Projet Mesi CrowdLending/CrowdLending/src/main/java/com/ipi201/crownlanding/CrownlandingApplication.java
@@ -0,0 +1,13 @@
+package com.ipi201.crownlanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrownlandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrownlandingApplication.class, args);
+	}
+
+}
---
+++ Projet Mesi CrowdLending/CrowdLending/src/test/java/com/ipi201/crownlanding/CrownlandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipi201.crownlanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrownlandingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
